7f1e680086bb30aa3d92dbef30b74d9f3a0cb9aa
mdmitriev <maksim_d@abbyy.com>
2017-06-27 12:25:10 +0300

    Implement the refactored approach from https://sourcemaking.com/design_patterns/chain_of_responsibility/java/1

--- src/com/client/Main.java
+++ src/com/client/Main.java
@@ -5,31 +5,47 @@ import java.util.Random;
 class Main {
 
     public static void main(String[] args) {
-        Handler[] nodes = {new Handler(), new Handler(),
-                new Handler(), new Handler()};
-        for (int i = 1, j; i < 6; i++) {
+        Handler rootChain = new Handler();
+        rootChain.add(new Handler());
+        rootChain.add(new Handler());
+        rootChain.add(new Handler());
+        rootChain.wrapAround(rootChain);
+        for (int i = 1; i < 6; i++) {
             System.out.println("Operation #" + i + ":");
-            j = 0;
-            while (!nodes[j].execute(i)) {
-                j = (j + 1) % nodes.length;
-            }
+            rootChain.execute(i);
             System.out.println();
         }
     }
 
     static class Handler {
-
-        private static final Random RANDOM = new Random();
+        private final static Random RANDOM = new Random();
         private static int nextID = 1;
         private int id = nextID++;
+        private Handler nextInChain;
 
-        public boolean execute(int num) {
+        public void add(Handler next) {
+            if (nextInChain == null) {
+                nextInChain = next;
+            } else {
+                nextInChain.add(next);
+            }
+        }
+
+        public void wrapAround(Handler root) {
+            if (nextInChain == null) {
+                nextInChain = root;
+            } else {
+                nextInChain.wrapAround(root);
+            }
+        }
+
+        public void execute(int num) {
             if (RANDOM.nextInt(4) != 0) {
                 System.out.println("   " + id + "-busy  ");
-                return false;
+                nextInChain.execute(num);
+            } else {
+                System.out.println(id + "-handled-" + num);
             }
-            System.out.println(id + "-handled-" + num);
-            return true;
         }
     }
 }
